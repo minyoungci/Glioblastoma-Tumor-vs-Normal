1e6c190ef9827e9b975dc34418b38bf9cccc4505
minyoungci <dbssus123@gmail.com>
2025-03-26 16:39:47 +0900

    Readme, R install

--- preprocessing.R
+++ preprocessing.R
@@ -1,15 +1,8 @@
-# R 코드 예시
+# 설치
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("DESeq2")
+
+# 로딩
 library(DESeq2)
-
-counts <- read.csv("combined_counts.csv", row.names = 1)
-condition <- factor(c("Tumor","Tumor","Tumor","Normal","Normal","Normal"))
-
-coldata <- data.frame(row.names=colnames(counts), condition=condition)
-
-dds <- DESeqDataSetFromMatrix(countData=counts, colData=coldata, design=~condition)
-dds <- DESeq(dds)
-
-res <- results(dds)
-res_df <- as.data.frame(res)
-
-write.csv(res_df, file="DESeq2_results.csv")
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Glioblastoma(교모세포종) 대상 RNA-seq 데이터 활용
+
+## Target 
+
+- Tumor vs Normal 세포 간의 차이를 RNA 수준에서 규명 
+
+- 질병 예측 및 진단 마커 발굴 (Biomarker Discovery)
+
+- 타겟 치료를 위한 생물학적 기전 이해 
+
+## Output
+
+- Biomarker candidate list: 암 예측 및 진단에 사용 가능한 후보 유전자 목록
+
+- 새로운 치료 타겟 후보: Glioblastoma 치료를 위한 타겟 가능성이 있는 유전자 또는 생물학적 경로 규명
+
+- 기계학습 기반 진단 예측 모델: RNA 데이터 기반 Glioblastoma 진단 모델 구축 가능
